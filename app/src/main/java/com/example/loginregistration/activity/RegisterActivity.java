b128b92546aacf7ace66c6f3b2b670fc4b6fd913
tomaszmajdan@gmail.com <lorat18son>
2017-06-30 10:53:38 +0200

    Utworzenie projektu

---
+++ app/src/main/java/com/example/loginregistration/activity/RegisterActivity.java
@@ -0,0 +1,8 @@
+package com.example.loginregistration.activity;
+
+/**
+ * Created by Tomasz.majdan on 2017-06-30.
+ */
+
+public class RegisterActivity {
+}
---
+++ app/src/main/java/com/example/loginregistration/app/AppConfig.java
@@ -0,0 +1,13 @@
+package com.example.loginregistration.app;
+
+/**
+ * Created by Tomasz.majdan on 2017-06-30.
+ */
+
+public class AppConfig {
+    // Server user login url
+    public static String URL_LOGIN = "http://192.168.0.102/android_login_api/login.php";
+
+    // Server user register url
+    public static String URL_REGISTER = "http://192.168.0.102/android_login_api/register.php";
+}
